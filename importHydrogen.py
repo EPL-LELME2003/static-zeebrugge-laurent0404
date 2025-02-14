719cc778eaaab8294a079fcbdd85e4a70a3b55ad
laurent0404 <laurent.jacquerye@student.uclouvain.be>
2025-02-14 16:02:50 +0100

    code fct

--- importHydrogen.py
+++ importHydrogen.py
@@ -10,36 +10,36 @@ model = pyo.ConcreteModel()
 ##########################################
 ############ CODE TO ADD HERE ############
 ##########################################
-LHV_NH3 = 18.5e6
-LHV_CH4 = 50e6
+model.LHV_NH3 = pyo.Param(initialize=18.5e6)
+model.LHV_CH4 = pyo.Param(initialize=50e6)
 
-rho_NH3 = 600
-rho_CH4 = 500
+model.rho_NH3 = pyo.Param(initialize=600)
+model.rho_CH4 = pyo.Param(initialize=500)
 
-losses_NH3 = 0.4
-losses_CH4 = 0.35
+model.losses_NH3 = pyo.Param(initialize=0.4)
+model.losses_CH4 = pyo.Param(initialize=0.35)
 
-boat_capacity = 2e5
-nbr_boat = 100
+model.boat_capacity = pyo.Param(initialize=2e5)
+model.nbr_boat = pyo.Param(initialize=100)
 
-H2_CH4 = 0.25
-CO2_CH4 = 2.75e3
-H2_NH3 = 0.18
+model.H2_CH4 = pyo.Param(initialize=0.25)
+model.CO2_CH4 = pyo.Param(initialize=2.75)
+model.H2_NH3 = pyo.Param(initialize=0.18)
 # Define model variables
 ##########################################
 ############ CODE TO ADD HERE ############
 ##########################################
 
-model.boat_NH3 = pyo.Var(within = pyo.NonNegativeIntegers, bounds = (0,nbr_boat))
+model.boat_NH3 = pyo.Var(within = pyo.NonNegativeIntegers, bounds = (0,model.nbr_boat))
 
-model.boat_CH4 = pyo.Var(within = pyo.NonNegativeIntegers, bounds = (0,nbr_boat))
+model.boat_CH4 = pyo.Var(within = pyo.NonNegativeIntegers, bounds = (0,model.nbr_boat))
 
 # Define the objective functions
 ##########################################
 ############ CODE TO ADD HERE ############
 ##########################################
 
-model.obj = pyo.Objective(expr = (rho_CH4*model.boat_CH4*H2_CH4 + rho_NH3*model.boat_NH3*H2_NH3)*boat_capacity, sense = pyo.maximize) 
+model.obj = pyo.Objective(expr = (model.rho_CH4*model.boat_CH4*model.H2_CH4 + model.rho_NH3*model.boat_NH3*model.H2_NH3)*model.boat_capacity, sense = pyo.maximize) 
 # Define the constraints
 ##########################################
 ############ CODE TO ADD HERE ############
@@ -47,9 +47,9 @@ model.obj = pyo.Objective(expr = (rho_CH4*model.boat_CH4*H2_CH4 + rho_NH3*model.
 
 model.con1 = pyo.Constraint(expr = (model.boat_NH3 + model.boat_CH4 <= 100))
 
-model.con2 = pyo.Constraint(expr = ((rho_CH4*model.boat_CH4*LHV_CH4/(1-losses_CH4) + rho_NH3*model.boat_NH3*LHV_NH3/(1-losses_NH3))*boat_capacity <= 140e12))
+model.con2 = pyo.Constraint(expr = ((model.rho_CH4*model.boat_CH4*model.LHV_CH4/(1-model.losses_CH4) + model.rho_NH3*model.boat_NH3*model.LHV_NH3/(1-model.losses_NH3))*model.boat_capacity <= 140e12*3600))
 
-model.con3 = pyo.Constraint(expr = (boat_capacity*rho_CH4*model.boat_CH4*CO2_CH4<= 14e6) )   
+model.con3 = pyo.Constraint(expr = (model.boat_capacity*model.rho_CH4*model.boat_CH4*model.CO2_CH4 <= 14e6*1e3) )   
 
 # Specify the path towards your solver (gurobi) file
 solver = pyo.SolverFactory("gurobi")
